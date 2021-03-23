717e42bea036b081181d8982ea62f9bf6c3448b6
409570092 <30064@gmail.com>
2021-03-23 15:59:54 +0800

    練習

---
+++ src/fju/edu/GraduateStudent.java
@@ -0,0 +1,7 @@
+package fju.edu;
+
+public class GraduateStudent {
+    public GraduateStudent(){
+        super();
+    }
+}
---
+++ src/fju/edu/Student.java
@@ -0,0 +1,11 @@
+package fju.edu;
+
+
+public class Student {
+    public static void main(String[] args) {
+        Customer cus = new Customer();
+        cus.print();
+        GraduateStudent gstu = new GraduateStudent();
+        gstu.print();
+    }
+}
